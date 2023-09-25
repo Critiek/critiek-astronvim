088e9285d7624ff475a6ebab6c7abfa6ca5deaae
Critiek <jumbra99@gmail.com>
2023-09-25 22:28:34 +0930

    NOT WORKING IN PROGRESS

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,6 @@
 return {
   -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
+  -- "AstroNvim/astrocommunity",
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,13 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  colorscheme = "catppuccin",
+
+  plugins = {
+    {
+      "catppuccin/nvim",
+      name = "catppuccin",
+      config = function()
+        require("catppuccin").setup {}
+      end,
+    },
+  },
 }
